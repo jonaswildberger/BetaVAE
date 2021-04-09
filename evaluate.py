1f69d2d7e58578c64811f22d4fc387f80b1d76ab
Mirofil <kawgawr@gmail.com>
2021-04-09 12:46:08 +0200

    Further reduced PCA sample size

--- evaluate.py
+++ evaluate.py
@@ -133,7 +133,7 @@ class Evaluator():
                     imgs_pca = np.reshape(data_imgs, (data_imgs.shape[0], data_imgs.shape[3]*data_imgs.shape[1]**2))
                 else: 
                     imgs_pca = np.reshape(dataset.imgs, (dataset.imgs.shape[0], dataset.imgs.shape[1]**2))
-                size = min(15000, len(imgs_pca))
+                size = min(10000, len(imgs_pca))
 
                 idx = np.random.randint(len(imgs_pca), size = size)
                 imgs_pca = imgs_pca[idx, :]       #not enough memory for full dataset -> repeat with random subsets               
