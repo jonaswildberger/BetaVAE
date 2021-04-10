19b33396998319226e67592b1f88b353522bade8
Mirofil <kawgawr@gmail.com>
2021-04-10 16:02:14 +0200

    Indent

--- utils/datasets.py
+++ utils/datasets.py
@@ -81,11 +81,9 @@ def get_dataloaders(dataset, root=None, shuffle=True, pin_memory=True,
                       **kwargs), dataset, DataLoader(dataset,
                       batch_size=1,
                       shuffle=True,
-                      ),
-                      DataLoader(dataset,
+                      ), DataLoader(dataset,
                       batch_size=batch_size,
-                      shuffle=False
-                      )
+                      shuffle=False)
 
 
 
