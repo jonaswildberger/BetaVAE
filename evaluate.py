601387f466e73bbe3e4b9c7e59aa2d132e5aec70
Mirofil <kawgawr@gmail.com>
2021-04-10 12:52:51 +0200

    Name bugs

--- evaluate.py
+++ evaluate.py
@@ -512,7 +512,7 @@ class Evaluator():
             for x, label in dataloader:
                 batch_size = x.size(0)
                 idcs = slice(n, n + batch_size)
-                q_zCx[idcs, :, 0], q_zCx[idcs, :, 1] = self.model.encoder(x.to(self.device))
+                q_zCx[idcs, :, 0], q_zCx[idcs, :, 1] = self.model.encode(x.to(self.device))
                 n += batch_size
 
         params_zCX = q_zCx.unbind(-1)
